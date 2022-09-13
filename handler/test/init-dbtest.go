196847cfd706cd9b4cb4a49a6545bdce9b15b637
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-09-13 16:35:30 -0300

    fix(initDBTest): Deixando DB pronto para inserção após o drop

--- handler/test/init-dbtest.go
+++ handler/test/init-dbtest.go
@@ -98,7 +98,28 @@ func dropDBTEST(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	t.Log("Database test dropped successfully")
+
+	createSchema := `CREATE TABLE IF NOT EXISTS Funcionarios(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(20),
+	sobrenome VARCHAR(60),
+	documento VARCHAR(11),
+	setor VARCHAR(20),
+	salario_bruto DECIMAL(10,2),
+	data_admissao DATE,
+	saude BOOLEAN,
+	dental BOOLEAN,
+	transporte BOOLEAN,
+	dependente INT,
+	pensao DECIMAL(10,2)
+);`
+	_, err = conn.Exec(createSchema)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 }
 
 func initDBTest(t *testing.T) {
